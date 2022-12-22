1d8cb3b1d86577fc412ea023789b99cf73ed0231
Boris.Obinyakov <andthenyouadd@gmail.com>
2022-12-22 15:15:42 +0300

    step 3, everything but order of + -

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { Command } from 'commander';
 import fs from 'fs';
+import _ from 'lodash';
+
+const readJSONFile = (path) => JSON.parse(fs.readFileSync(path));
+const getEntriesFromFile = (path) => _.toPairs(readJSONFile(path)).sort();
+const getExtension = (path) => path.split('.').at(-1);
 
 export const programGendiff = new Command();
 
@@ -11,16 +16,24 @@ programGendiff
   .argument('<filepath2>', 'path to second file')
   .option('-f, --format <type>', 'output format')
   .action(function () {
-    console.log(readJSONFile(this.args[0]));
-    console.log(readJSONFile(this.args[1]));
+    const entries1 = getEntriesFromFile(this.args[0]);
+    const entries2 = getEntriesFromFile(this.args[1]);
+
+    const expelledEntries = _.differenceWith(entries1, entries2, _.isEqual);
+    const newEntries = _.differenceWith(entries2, entries1, _.isEqual);
+    const unchangedEntries = _.intersectionWith(entries1, entries2, _.isEqual);
+
+    expelledEntries.forEach((el) => el.push('-'));
+    newEntries.forEach((el) => el.push('+'));
+    unchangedEntries.forEach((el) => el.push(' '));
+
+    const summary = [...expelledEntries, ...newEntries, ...unchangedEntries].sort();
+
+    const getDiffLine = (entry) => `${entry[2]} ${entry[0]}: ${entry[1]}`;
+
+    console.log('{');
+    summary.forEach((el) => console.log(getDiffLine(el)));
+    console.log('}');
   });
 
 programGendiff.parse();
-
-function readJSONFile(path) {
-  return JSON.parse(fs.readFileSync(path));
-}
-
-function getExtension(path) {
-  return path.split('.').at(-1);
-}
