066085694467821ddfb37e978eedb37ad31e51ad
sirflyingv <andthenyouadd@gmail.com>
2023-01-18 21:42:53 +0300

    no custom error

--- formatters/index.js
+++ formatters/index.js
@@ -2,7 +2,7 @@ import formatPlain from './formatPlain.js';
 import formatStylish from './formatStylish.js';
 
 export default (formatName) => {
-  if (formatName === 'stylish') return formatStylish;
   if (formatName === 'plain') return formatPlain;
   if (formatName === 'json') return JSON.stringify;
+  return formatStylish;
 };
