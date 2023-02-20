324c62ecfdcbe84b4e59780a2e79fa12845039c4
sirflyingv <andthenyouadd@gmail.com>
2023-02-20 13:08:17 +0300

    unchanged nested objects now get into diff right

--- src/genDiffData.js
+++ src/genDiffData.js
@@ -6,6 +6,9 @@ const genDiffData = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
+    const valuesAreEqual = _.isEqual(value1, value2);
+    if (valuesAreEqual) return { key, type: 'unchanged', value: value1 };
+
     const bothValuesAreObjects = isTrueObj(value1) && isTrueObj(value2);
     if (bothValuesAreObjects) {
       return { key, type: 'nested', children: genDiffData(value1, value2) };
@@ -16,9 +19,6 @@ const genDiffData = (data1, data2) => {
 
     const keyIsDeleted = _.has(data1, key) && !_.has(data2, key);
     if (keyIsDeleted) return { key, type: 'deleted', value: value1 };
-
-    const valuesAreEqual = _.isEqual(value1, value2);
-    if (valuesAreEqual) return { key, type: 'unchanged', value: value1 };
 
     return {
       key,
